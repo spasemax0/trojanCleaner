974bda98f90d5856d83d4682cb16cfc0ad1f357c
spasemax0 <123032891+spasemax0@users.noreply.github.com>
2023-03-19 18:39:25 -0600

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,28 @@
+#author: Spase Sandoval
+#bash trojan bomb for linux systems, dont use on personal without backup
+
+#!/bin/bash
+
+# Step 1: Identify the Trojan virus
+trojan_process=$(ps aux | grep -E 'malicious_process_name' | grep -v grep)
+
+# Step 2: Stop the malicious process
+if [ -n "$trojan_process" ]
+then
+    echo "Stopping malicious process: $trojan_process"
+    kill $(echo $trojan_process | awk '{print $2}')
+fi
+
+# Step 3: Delete the Trojan executable
+trojan_executable=$(find / -name 'malicious_executable_name' 2>/dev/null)
+if [ -n "$trojan_executable" ]
+then
+    echo "Deleting Trojan executable: $trojan_executable"
+    rm $trojan_executable
+fi
+
+# Step 4: Remove the Trojan's persistence mechanism
+# customize this section based on the specific Trojan virus you are targeting
+
+# Step 5: Clean up any additional artifacts
+# customize this section based on the specific Trojan virus you are targeting
